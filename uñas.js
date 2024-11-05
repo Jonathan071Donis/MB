070b98f2c36fe3c4911871c45ec41aeb94f6ee8b
Jonathan <jonathandonis03@gmail.com>
2024-11-05 16:23:55 -0600

    finales

--- uñas.js
+++ uñas.js
@@ -2,10 +2,16 @@ function showModal(imageSrc, description) {
     const modal = document.getElementById("modal");
     const modalImg = document.getElementById("modalImg");
     const modalDescription = document.getElementById("modalDescription");
+    const whatsappLink = document.getElementById("whatsappLink");
 
-    modal.style.display = "block";
+    modal.style.display = "flex";
     modalImg.src = imageSrc;
     modalDescription.innerText = description;
+
+    // Construir el enlace de WhatsApp
+    const phoneNumber = "50246058198"; // Reemplaza con el número de WhatsApp
+    const message = `Hola, quiero consultar sobre: ${description}`;
+    whatsappLink.href = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
     // Cerrar el modal si se hace clic fuera de la imagen
     modal.addEventListener("click", function(event) {
